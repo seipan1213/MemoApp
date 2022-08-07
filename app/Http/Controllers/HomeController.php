01a9fe013214c31f699da9cb916c100e284014ec
seipan1213 <seiju1213@gmail.com>
2022-08-07 17:54:13 +0900

    tag 実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Memo;
+use App\Models\MemoTag;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -26,31 +29,83 @@ class HomeController extends Controller
     public function index()
     {
         $memos = Memo::select('memos.*')->where('user_id', '=', Auth::id())->whereNUll('deleted_at')->orderBy('updated_at', 'DESC')->get();
-        return view('create',compact('memos'));
+
+        $tags = Tag::where('user_id', '=', Auth::id())->whereNull('deleted_at')->orderBy('id', 'DESC')->get();
+        return view('create',compact('memos','tags'));
     }
 
     public function store(Request $request)
     {
         $posts = $request->all();
 
-        Memo::insert(['content'=>$posts['content'],'user_id'=>Auth::id()]);
+        DB::transaction(function () use($posts) {
+            $memo_id = Memo::insertGetId(['content' => $posts['content'], 'user_id' => Auth::id()]);
+            $tag_exists = Tag::where('user_id', '=', Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+
+            if ((!empty($posts['new_tag']) || $posts['new_tag'] === "0")&& !$tag_exists){
+                $tag_id = Tag::insertGetId(['user_id'=>Auth::id(), 'name' => $posts['new_tag']]);
+                MemoTag::insert(['memo_id' => $memo_id, 'tag_id' => $tag_id]);
+            }
+            if(!empty($posts['tags'][0])){
+                foreach($posts['tags'] as $tag){
+                    MemoTag::insert(['memo_id'=> $memo_id, 'tag_id' => $tag]);
+                }
+            }
+        });
         return redirect(route('home'));
     }
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')->where('user_id', '=', Auth::id())->whereNUll('deleted_at')->orderBy('updated_at', 'DESC')->get();
+        $memos = Memo::select('memos.*')
+        ->where('user_id', '=', Auth::id())
+        ->whereNUll('deleted_at')
+        ->orderBy('updated_at', 'DESC')
+        ->get();
 
-        $edit_memo = Memo::find($id);
+        $edit_memo = Memo::select('memos.*', 'tags.id AS tag_id')
+        ->leftJoin('memo_tags', 'memo_tags.memo_id' ,'=', 'memos.id')
+        ->leftJoin('tags', 'memo_tags.tag_id', '=', 'tags.id')
+        ->where('memos.user_id', '=', Auth::id())
+        ->where('memos.id','=',$id)
+        ->whereNull('memos.deleted_at')
+        ->get();
 
-        return view('edit',compact('memos','edit_memo'));
+        $include_tags = [];
+
+        foreach($edit_memo as $memo){
+            array_push($include_tags,$memo['tag_id']);
+        }
+
+        $tags = Tag::where('user_id', '=', Auth::id())
+        ->whereNull('deleted_at')
+        ->orderBy('id', 'DESC')
+        ->get();
+        return view('edit',compact('memos','edit_memo','include_tags','tags'));
     }
 
     public function update(Request $request)
     {
         $posts = $request->all();
 
-        Memo::where('id', $posts['memo_id'])-> update(['content'=>$posts['content']]);
+        DB::transaction(function () use($posts) {
+            Memo::where('id', $posts['memo_id'])-> update(['content'=>$posts['content']]);
+            MemoTag::where('memo_id', '=', $posts['memo_id'])->delete();
+
+            if (!empty($posts['tags'])){
+                foreach($posts['tags'] as $tag){
+                    MemoTag::insert(['memo_id' =>$posts['memo_id'], 'tag_id' => $tag]);
+                }
+            }
+
+            $tag_exists = Tag::where('user_id', '=', Auth::id())->where('name', '=', $posts['new_tag'])->exists();
+
+            if ((!empty($posts['new_tag']) || $posts['new_tag'] === "0") && !$tag_exists){
+                $tag_id = Tag::insertGetId(['user_id'=>Auth::id(), 'name' => $posts['new_tag']]);
+                MemoTag::insert(['memo_id' => $posts['memo_id'], 'tag_id' => $tag_id]);
+            }
+        });
+
         return redirect(route('home'));
     }
 
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,16 +5,23 @@
     <div class="card-header">メモ編集
         <form class="card-body" action="{{ route('destroy') }}" method="POST">
             @csrf
-            <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}"/>
+            <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}"/>
             <button type="submit" class="btn btn-primary">削除</button>
         </form>
     </div>
     <form class="card-body" action="{{ route('update') }}" method="POST">
         @csrf
-        <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}"/>
+        <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}"/>
         <div class="form-group">
-            <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo['content'] }}</textarea>
+            <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
+            @foreach ($tags as $tag)
+            <div class="form-check form-check-inline mb-3">
+                <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : ''}}>
+                <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+            </div>
+            @endforeach
         </div>
+        <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力"/>
         <button type="submit" class="btn btn-primary">更新</button>
     </form>
 </div>
